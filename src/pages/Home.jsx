064bd5f602a80545f2ddc6883ebde07cdef6421c
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-08-31 23:27:17 +0600

    Fixed exercise data on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,24 @@ import { Box } from '@mui/material';
 import HeroBanner from '../components/HeroBanner';
 import SearchExercises from '../components/SearchExercises';
 import Exercises from '../components/Exercises';
+import { useState } from 'react';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
